5eeefba5f8b06acf5bbdb3570938f08f49c05f41
Kao <zichongkao@gmail.com>
2013-07-27 09:31:18 -0700

    html builder added

--- server2.js
+++ server2.js
@@ -1,41 +1,88 @@
 var util = require('util'),
     express = require('express'),
-    events = require('events'),
+    stylus = require('stylus'),
+	nib = require('nib'),
     fs = require('fs'),
+	events = require('events'),
     port = 8080;
 
 var app = module.exports = express();
+
 util.puts('Listening on ' + port + '...');
+app.use(express.logger('dev'));
 util.puts('Press Ctrl + C to stop.');
+app.use(express.static(__dirname + '/frontend'));
+
 app.use(express.bodyParser());
 
 app.get('/',function(req,res){
 	res.sendfile(__dirname + '/frontend/index.html');
 });
-app.listen(8080);
+app.listen(port);
 
 // page 1 ------------------------------
-// place handleDomain function inside handleDomain variable
-// so that it can be called eventEmitter
-//$('form[name="search"]').on('domainSelected',function(e,domain){
-//	console.log(dmain);
-//});
+
+app.post('/domain', function (req, res) {
+    console.log(req.body.name); 
+});
+
+// page 2 ------------------------------
+app.post('/platform', function (req, res) {
+    console.log(req.body.platform);
+	console.log(req.body.email);
+	console.log(req.body.password);
+	console.log(req.body.servername);
+});
 
 
-var data = ""
 
-fs.writeFile(__dirname + "/package/index.html", data, function(err) {
-    if(err) {
+
+// page 4
+//prepare index.html --------------------
+
+// d3 
+var d3 = "<script src="http://d3js.org/d3.v3.min.js" charset="utf-8"></script>"
+fs.appendFile(__dirname + "/package/index.html", d3, function(err, fd){
+	if(err) {
         console.log(err);
     } else {
-        console.log("The file was saved!");
+        console.log(fd + " d3 tags added successfully.");
     }
 });
 
-//fs.open("/package/index.html", '</body> \n </html>', 0666, function(err, fd){
-//	if(err) {
-//        console.log(err);
-//    } else {
-//        console.log(fd + " end tags added successfully.");
-//    }
-//});
+// insert </head> <body>
+fs.appendFile(__dirname + "/package/index.html", '\n</head>\n<body>', function(err, fd){
+	if(err) {
+        console.log(err);
+    } else {
+        console.log(fd + " /head, body tags added successfully.");
+    }
+});
+
+
+// goog analytics boilder plate
+var goog = " \
+\n<!-- Google Analytics: change UA-XXXXX-X to be your site's ID. --> \n\
+<script>\n\
+	var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];\n\
+	(function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];\n\
+	g.src='//www.google-analytics.com/ga.js';\n\
+	s.parentNode.insertBefore(g,s)}(document,'script'));\n\
+</script>"
+
+fs.appendFile(__dirname + "/package/index.html", goog, function(err, fd){
+	if(err) {
+        console.log(err);
+    } else {
+        console.log(fd + " google analytics snippet added successfully.");
+    }
+});
+
+// final tags
+fs.appendFile(__dirname + "/package/index.html", '\n</body>\n</html>', function(err, fd){
+	if(err) {
+        console.log(err);
+    } else {
+        console.log(fd + " end tags added successfully.");
+    }
+});
